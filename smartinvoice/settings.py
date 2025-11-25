1d91ebd8ae79be7f87756ebb48be76ee1adf35de
rishabh <realme@rishabh.com>
2025-11-25 23:13:09 +0530

    Force allow all hosts to fix deployment error

--- smartinvoice/settings.py
+++ smartinvoice/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5udji+%=19&cd=8626m$c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
